e38f42ed44b4e615375dc7a63b08b33e63a38ea9
DefiCake <reche@0xb.one>
2022-05-11 14:38:48 +0200

    Still nothing

--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -1,8 +1,10 @@
 // If your plugin extends types from another plugin, you should import the plugin here.
 
 // To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
-import { JsonRpcServer } from "hardhat/types";
+import "hardhat/types/runtime";
+import "hardhat/types/config";
 import { EnsMockConfig } from "../types";
+import { JsonRpcServer } from "hardhat/types";
 
 declare module "hardhat/types/config" {
   interface HardhatNetworkConfig {
