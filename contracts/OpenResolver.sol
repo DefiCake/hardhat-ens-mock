d9950a892eddfb350492eed9ba9c49eaf335a767
DefiCake <rarore92@gmail.com>
2023-08-28 18:15:18 +0200

    feat: add OpenResolver contract

---
+++ contracts/OpenResolver.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@ensdomains/ens-contracts/contracts/resolvers/profiles/ABIResolver.sol";
+import "@ensdomains/ens-contracts/contracts/resolvers/profiles/AddrResolver.sol";
+import "@ensdomains/ens-contracts/contracts/resolvers/profiles/ContentHashResolver.sol";
+import "@ensdomains/ens-contracts/contracts/resolvers/profiles/DNSResolver.sol";
+import "@ensdomains/ens-contracts/contracts/resolvers/profiles/InterfaceResolver.sol";
+import "@ensdomains/ens-contracts/contracts/resolvers/profiles/NameResolver.sol";
+import "@ensdomains/ens-contracts/contracts/resolvers/profiles/PubkeyResolver.sol";
+import "@ensdomains/ens-contracts/contracts/resolvers/profiles/TextResolver.sol";
+import "@ensdomains/ens-contracts/contracts/resolvers/profiles/ExtendedResolver.sol";
+
+/**
+ * A simple resolver anyone can use; allows any address to set any node
+ */
+contract OpenResolver is
+  Ownable,
+  ABIResolver,
+  AddrResolver,
+  ContentHashResolver,
+  DNSResolver,
+  InterfaceResolver,
+  NameResolver,
+  PubkeyResolver,
+  TextResolver,
+  ExtendedResolver
+{
+  function isAuthorised(bytes32) internal view override returns (bool) {
+    return msg.sender == owner();
+  }
+
+  function supportsInterface(
+    bytes4 interfaceID
+  )
+    public
+    view
+    virtual
+    override(
+      ABIResolver,
+      AddrResolver,
+      ContentHashResolver,
+      DNSResolver,
+      InterfaceResolver,
+      NameResolver,
+      PubkeyResolver,
+      TextResolver
+    )
+    returns (bool)
+  {
+    return super.supportsInterface(interfaceID);
+  }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/types";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+  },
+};
+
+export default config;
